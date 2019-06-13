2ee2d74818fbf339d1fda49b0a0c50908176afdf
Rodrigo Augusto Girani Tejos <rodrigo.gtejos@gmail.com>
2019-06-13 18:59:23 +0000

    ImageController.js change aws region from us-east-1 to sa-east-1

--- server/src/controllers/ImageController.js
+++ server/src/controllers/ImageController.js
@@ -3,7 +3,7 @@ const Post = require('../models/Post');
 // Load the AWS SDK for Node.js
 const AWS = require('aws-sdk');
 // Set the region 
-AWS.config.update({region: 'us-west-1'});
+AWS.config.update({region: 'sa-east-1'});
 
 // Create S3 service object
 s3 = new AWS.S3({apiVersion: '2006-03-01'});
