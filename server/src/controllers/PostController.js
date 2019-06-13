369f0dd1e3720649da8f675bf57bb0d8ed4da982
Rodrigo Augusto Girani Tejos <rodrigo.gtejos@gmail.com>
2019-06-13 19:04:16 +0000

    PostController.js change bucket name and region

--- server/src/controllers/PostController.js
+++ server/src/controllers/PostController.js
@@ -8,12 +8,12 @@ const fs = require('fs')
 const AWS = require('aws-sdk');
 
 // Set the region 
-AWS.config.update({region: 'sa-east-1'});
+AWS.config.update({region: 'us-east-1'});
 
 // Create S3 service object
 s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
-bucket = "instagram-resized";
+bucket = "instagram-sp-2";
 
 // call S3 to retrieve upload file to specified bucket
 var uploadParams = {Bucket: bucket, Key: '', Body: ''};
